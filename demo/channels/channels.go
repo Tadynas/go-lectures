c86ff2dce452e8473ed15bb135e505050d3d187a
Tadynas <griciustadas@gmail.com>
2022-08-11 08:42:03 +0300

    channels feature

--- demo/channels/channels.go
+++ demo/channels/channels.go
@@ -1,8 +1,63 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+type ControlMessage int
+
+type Job struct {
+	data   int
+	result int
+}
+
+const (
+	DoExit = iota
+	ExitOk
+)
+
+func doubler(jobs, results chan Job, control chan ControlMessage) {
+	for {
+		select {
+		case msg := <-control:
+			switch msg {
+			case DoExit:
+				fmt.Println("Exiting goroutine")
+				control <- ExitOk
+				return
+			default:
+				panic("Unhandled control message")
+			}
+		case job := <-jobs:
+			results <- Job{data: job.data, result: job.data * 2}
+		default:
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
 
 func main() {
+	jobs := make(chan Job, 50)
+	results := make(chan Job, 50)
+	control := make(chan ControlMessage)
 
+	go doubler(jobs, results, control)
+
+	for i := 0; i < 30; i++ {
+		jobs <- Job{i, 0}
+	}
+
+	for {
+		select {
+		case result := <-results:
+			fmt.Println(result)
+		case <-time.After(500 * time.Millisecond):
+			fmt.Println("timed out")
+			control <- DoExit
+			<-control
+			fmt.Println("program exit")
+			return
+		}
+	}
 }
